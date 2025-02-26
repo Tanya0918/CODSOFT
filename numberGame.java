49dc79ddae86ae2bab40fc9737473d1dad1e9721
Tanya0918 <tanyavar0912@gmail.com>
2025-02-26 22:29:20 +0530

    Task 1 completion

--- numberGame.java
+++ numberGame.java
@@ -1,5 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class numberGame {
     public static void main(String[] args) {
         System.out.println("Number Game");
+        Random randomNum = new Random();
+        Scanner input = new Scanner(System.in);
+        int targetNum = randomNum.nextInt(100);
+        int Attempt=0;
+        boolean playGame=true;
+
+        while(playGame){
+        while(Attempt<5){
+        System.out.print("Enter your guessed number: ");
+        int guess = input.nextInt();
+        if(guess==targetNum)
+        {
+            System.out.println("Yeahh...you guessed the correct number.");
+        }
+        else{
+            System.out.println("Ohh sorry...you guessed the wrong.");
+            if(guess<targetNum){
+                System.out.println("you guessed the lower number. Try Again!");
+            }
+            else{
+                System.out.println("you guessed the upper number. Try Again!");
+            }
+        }
+        System.out.print("Do you want to play this game: ");
+        String playAgain = input.next();
+        if(playAgain.equals("no")){
+            System.out.println("Thanks for playing!!");
+            playGame=false;
+        }
+        Attempt++;
+    }
+    if(Attempt==5){
+        System.out.println("Sorry...you are out of attempts.\nThe target number is " + targetNum);
+        break;
+    }
+}
+
+
     }
 }
