ed7c8670ef881d1b40e219c1d75a84b66c9a2047
Tanya0918 <tanyavar0912@gmail.com>
2025-02-26 23:14:25 +0530

    Updated

--- numberGame.java
+++ numberGame.java
@@ -4,34 +4,30 @@ import java.util.Scanner;
 public class numberGame {
     public static void main(String[] args) {
         System.out.println("Number Game");
-        Random randomNum = new Random();
         Scanner input = new Scanner(System.in);
-        int targetNum = randomNum.nextInt(100);
         int Attempt=0;
         boolean playGame=true;
 
-        while(playGame && Attempt<5){
+        while(playGame){
+            Random randomNum = new Random();
+            int targetNum = randomNum.nextInt(100);
+            while(Attempt<5){
         System.out.print("Enter your guessed number: ");
         int guess = input.nextInt();
         if(guess==targetNum)
         {
-            System.out.println("Yeahh...you guessed the correct number.");
+            Attempt+=1;
+            System.out.println("Yeahh...you guessed the correct number in " + Attempt + "attempts.");
             break;
         }
         else{
-            System.out.println("Ohh sorry...you guessed the wrong.");
+            System.out.println("Ohh sorry...you guessed the wrong number.");
             if(guess<targetNum){
-                System.out.println("you guessed the lower number. Try Again!" + targetNum);
+                System.out.println("you guessed too low. Try Again!");
             }
             else{
-                System.out.println("you guessed the upper number. Try Again!");
+                System.out.println("you guessed too high. Try Again!");
             }
-        }
-        System.out.print("Do you want to play this game: ");
-        String playAgain = input.next();
-        if(playAgain.equals("no")){
-            System.out.println("Thanks for playing!!");
-            break;
         }
         Attempt++;
        
@@ -39,6 +35,14 @@ public class numberGame {
         System.out.println("Sorry...you are out of attempts.\nThe target number is " + targetNum);
         break;
     }
+}
+System.out.print("\nDo you want to play this game: ");
+String playAgain = input.next();
+Attempt=0;
+if(playAgain.equals("no")){
+    System.out.println("Thanks for playing!!");
+    break;
+}
 }
     }
 }
